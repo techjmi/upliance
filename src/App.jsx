2503e1dfbfc8c70f25eff5607977ff813d6dd4cd
techjmi <contactshamim62@gmail.com>
2025-02-06 04:52:55 +0530

    domm

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Counter from './component/Counter'
-import TextEditor from './component/TextEditor'
-import Navbar from './component/Navbar'
-import { Route, Router, Routes } from 'react-router'
-import Dashboard from './pages/Dashboard'
-import Footer from './component/Footer'
-import Form from './component/Form'
-import Signup from './pages/Signup'
-import Login from './pages/Login'
-import PrivateRoute from './component/PrivateRoute'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import Service from './pages/Service'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Counter from "./component/Counter";
+import TextEditor from "./component/TextEditor";
+import Navbar from "./component/Navbar";
+import { Route, Router, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Footer from "./component/Footer";
+import Form from "./component/Form";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import PrivateRoute from "./component/PrivateRoute";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Service from "./pages/Service";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Navbar />
-    <Routes>
-<Route element={<PrivateRoute />}>
-<Route path='/dashboard' element={<Dashboard />} />
-</Route>
-    <Route path='/' element={<Counter/>} />
-      <Route path='/editor' element={<TextEditor />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/services' element={<Service />} />
-    </Routes>
-    <Footer />
-    {/* <Counter /> */}
-   
+      <Navbar />
+      <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/" element={<Counter />} />
+        <Route path="/editor" element={<TextEditor />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Service />} />
+      </Routes>
+      <Footer />
+      {/* <Counter /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom'; // Corrected import
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-   {/* <StrictMode> */}
-    <App />
-  {/* </StrictMode>, */}
+    {/* <StrictMode> */}
+      <App />
+    {/* </StrictMode> */}
   </BrowserRouter>
- 
-)
+);
